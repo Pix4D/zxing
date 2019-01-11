8c3fa8130f15a26db90bc28b11fac427ce637902
Mohamed-Ghaith Kaabi <mohamed.kaabi@pix4d.com>
2019-01-11 11:54:59 +0100

    Disable ICONV support

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 class ZxingConan(ConanFile):
     name = 'zxing'
     lib_version = '3.3.3'
-    revision = '3'
+    revision = '5'
     version = '{}-{}'.format(lib_version, revision)
     settings = 'os', 'compiler', 'build_type', 'arch'
     description = 'Zxing recipe'
